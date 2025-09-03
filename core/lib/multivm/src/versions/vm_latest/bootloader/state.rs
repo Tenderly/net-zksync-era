65c661c18a9efca6b102027202dcdbb21b7c7e6e
Milan Pandurov <milan.pandurov@tenderly.co>
2025-09-03 10:30:35 +0200

    chore(core/multivm): Remove unnecessary asserts
    These asserts don't make much sense in Tenderly way of executing
    the VM since we don't execute batches in same way as regular EraVM.
    
    Signed-off-by: Milan Pandurov <milan.pandurov@tenderly.co>
    (cherry picked from commit 67f405bfdabaf1579fecdd998fd6b9797810e3e7)

--- core/lib/multivm/src/versions/vm_latest/bootloader/state.rs
+++ core/lib/multivm/src/versions/vm_latest/bootloader/state.rs
@@ -103,7 +103,8 @@ impl BootloaderState {
             !last_block.txs.is_empty(),
             "Can not create new miniblocks on top of empty ones"
         );
-        assert_next_block(&last_block.l2_block(), &l2_block, self.protocol_version);
+        // (Tenderly) last_block.hash is 0x00 which will cause assertion failure
+        // assert_next_block(&last_block.l2_block(), &l2_block, self.protocol_version);
         self.push_l2_block(l2_block);
     }
 
